e8e9e322195689bca6a09e93b55a83401ca95e79
unknown <shiveshtiwari0@gmail.com>
2023-11-22 16:04:19 +0530

    small changes

--- Home.py
+++ Home.py
@@ -2,5 +2,6 @@ import streamlit as st
 
 st.title("Home Page")
 
-st.text_input(label="OpenAI Api", placeholder="Enter your OpenAI API Key")
+# st.text_input(label="OpenAI Api", placeholder="Enter your OpenAI API Key")
+# INPLEMENT USING PaLM2
 st.text_input(label="Title", placeholder="Enter title")
