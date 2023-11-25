ff7b2ce187e0da0b5d7fa6a0b1633469ca9379ca
unknown <shiveshtiwari0@gmail.com>
2023-11-25 09:38:05 +0530

    changes

--- Home.py
+++ Home.py
@@ -1,7 +1,16 @@
 import streamlit as st
+import google.generativeai as palm
 
 st.title("Home Page")
 
+palm_api_key = ""
+
 # st.text_input(label="OpenAI Api", placeholder="Enter your OpenAI API Key")
 # INPLEMENT USING PaLM2
-st.text_input(label="Title", placeholder="Enter title")
+course_name = st.text_input(label="Title", placeholder="Enter title")
+
+if course_name:
+    palm.configure(api_key=palm_api_key)
+    res = palm.chat(messages=[course_name])
+
+    st.write(res.last)
